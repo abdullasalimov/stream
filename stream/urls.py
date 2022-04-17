ad0778baf5e3d20fbabd517b25544dcbc589d3a2
unknown <abdullasalimov7@gmail.com>
2022-04-18 02:11:03 +0500

    1/2 final

--- stream/urls.py
+++ stream/urls.py
@@ -6,4 +6,17 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('blog_index', blog_index, name='blog_index'),
+    path('blog_post', blog_post, name='blog_post'),
+    path('news_index', news_index, name='news_index'),
+    path('news_single', news_single, name='news_single'),
+    path('speaking', speaking, name='speaking'),
+    path('listening_single', listening_single, name='listening_single'),
+    path('listening_video', listening_video, name='listening_video'),
+    path('writing_index', writing_index, name='writing_index'),
+    path('writing_newspost', writing_newspost, name='writing_newspost'),
+    path('writing_post', writing_post, name='writing_post'),
+    path('reading_index', reading_index, name='reading_index'),
+    path('reading_detail', reading_detail, name='reading_detail'),
+    path('reading_listing', reading_listing, name='reading_listing'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stream/views.py
+++ stream/views.py
@@ -3,3 +3,42 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def blog_index(request):
+    return render(request, 'blog/index.html')
+
+def blog_post(request):
+    return render(request, 'blog/post.html')
+
+def news_index(request):
+    return render(request, 'news/index.html')
+
+def news_single(request):
+    return render(request, 'news/single-page.html')
+
+def speaking(request):
+    return render(request, 'upload.html')
+
+def listening_single(request):
+    return render(request, 'listening/single-channel.html')
+
+def listening_video(request):
+    return render(request, 'listening/video-page.html')
+
+def writing_index(request):
+    return render(request, 'writing/index.html')
+
+def writing_newspost(request):
+    return render(request, 'writing/newspost.html')
+
+def writing_post(request):
+    return render(request, 'writing/post.html')
+
+def reading_index(request):
+    return render(request, 'reading/index.html')
+
+def reading_detail(request):
+    return render(request, 'reading/detail.html')
+
+def reading_listing(request):
+    return render(request, 'reading/listing.html')
